84e6fc429b5670bf5054f44a017929d125bbafd6
BranimirNet <branimir.netrebasic@gmail.com>
2025-12-01 18:27:06 +0100

    Drugi komit

---
+++ AutoApp/Models/CarCollection.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace AutoApp\Models;
+
+use Iterator;
+
+class CarCollection implements Iterator {
+
+    private array $cars = [];
+    private int $position = 0;
+
+    public function add(Car $car): void {
+        $this->cars[] = $car;
+    }
+
+    public function current(): Car {
+        return $this->cars[$this->position];
+    }
+
+    public function key(): int {
+        return $this->position;
+    }
+
+    public function next(): void {
+        $this->position++;
+    }
+
+    public function rewind(): void {
+        $this->position = 0;
+    }
+
+    public function valid(): bool {
+        return isset($this->cars[$this->position]);
+    }
+
+    public function count(): int {
+        return count($this->cars);
+    }
+}
+
+
+
+
+
+
+
+
+?>
---
+++ AutoApp/Models/CarType.php
@@ -0,0 +1,22 @@
+<?php 
+
+namespace AutoApp\Models;
+
+class CarType{
+
+    public function __construct(
+        public int $id,
+        public string $name,
+       
+    )
+    {}
+    }
+
+    
+
+
+
+
+
+
+?>
