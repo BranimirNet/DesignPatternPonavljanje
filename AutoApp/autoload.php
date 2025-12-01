6ac72624071ab0042407be17d10839931f2b8ee2
BranimirNet <branimir.netrebasic@gmail.com>
2025-12-01 19:11:41 +0100

    Ispravljen autoloader

--- AutoApp/autoload.php
+++ AutoApp/autoload.php
@@ -1,23 +1,18 @@
 <?php
 
-spl_autoload_register(function ($className): void{
-    if(strpos($className, 'AutoApp\\') != 0){
+spl_autoload_register(function ($className): void {
+
+    if (strpos($className, 'AutoApp\\') === 0) {
+
         $relativeClass = substr($className, strlen('AutoApp\\'));
+
         $putanja = __DIR__ . '/' . str_replace('\\', '/', $relativeClass) . '.php';
-        if(file_exists($putanja)){
+
+        if (file_exists($putanja)) {
             require_once $putanja;
+        } else {
+            echo "Datoteka za klasu {$className} ne postoji. Tražena putanja: {$putanja}";
         }
-        else{
-            echo "Datoteka za klasu {$className} ne postoji.";
-        }
-}
-}
-    
-);
+    }
 
-
-
-
-
-
-?>
+});
